384c29b953c6123cc0976357344511b4a52361e1
Шаломов Евгений Александрович <shalomov@triol>
2020-06-12 16:35:14 +0300

    code review

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 
-import state from './redux/state';
+import store from './redux/state';
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './rerender';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+ let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state} addPost={store.addPost.bind(store)} updateNewPostText= {store.updateNewPostText.bind(store) } addMsg={store.addMsg.bind(store)}  updateNewMessage= { store.updateNewMessage.bind(store) } />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 
 
-rerenderEntireTree(state);
 
+rerenderEntireTree(store.getState());
 
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,119 +1,127 @@
-import { rerenderEntireTree } from "../rerender";
+let store = {
+  _state : {
+    profilePage: {
+      postsData: [
+        {
+          id: 1,
+          message: "Hi, how areu?",
+          like: "122",
+        },
+        {
+          id: 2,
+          message: "I'm fine and you?",
+          like: "22",
+        },
+      ],
+      newPostText: ''
+    },
+    dialogsPage: {
+      messages: [
+        {
+          id: 1,
+          message: "H1",
+          photo: 'url(https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/reference_guide/cats_and_excessive_meowing_ref_guide/1800x1200_cats_and_excessive_meowing_ref_guide.jpg)'
+        },
+        {
+          id: 2,
+          message: "Hello!",
+          photo: 'url(https://focus.ua/storage/pub/images/2015/grumpy-cat_realgrumpycat-Instagram.jpg)'
+        },
+        {
+          id: 3,
+          message: "Haudi",
+          photo: 'url(https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg)'
+        },
+      ],
+      newMessage: '',
+      dialogs: [
+        {
+          id: 1,
+          name: "EE",
+        },
+        {
+          id: 2,
+          name: "JJ",
+        },
+        {
+          id: 3,
+          name: "WS",
+        },
+      ],
+    },
+    aside : {
+      title : [
+        {
+          titlefriends: 'Best friends'
+        }
+      ],
+      friends : [
+        {
+          name: 'Chandler',
+          photo: 'https://post.healthline.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg'
+        },
+        {
+          name: 'Joey',
+          photo: 'https://www.cesarsway.com/wp-content/uploads/2019/10/AdobeStock_190562703.jpeg'
+        },
+        {
+          name: 'Ross',
+          photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/03/26125843/yorkshire-terrier-cover-500x486.jpg'
+        },
+        {
+          name: 'Gunther',
+          photo: 'https://cdn.cnn.com/cnnnext/dam/assets/180316113418-travel-with-a-dog-3-super-169.jpeg'
+        }
+      ]
+    }
+  },
+  getState() {
+    return this._state;
+  },
 
-let state = {
-  profilePage: {
-    postsData: [
-      {
-        id: 1,
-        message: "Hi, how areu?",
-        like: "122",
-      },
-      {
-        id: 2,
-        message: "I'm fine and you?",
-        like: "22",
-      },
-    ],
-    newPostText: ''
+  _callSubscriber () {
+
   },
-  dialogsPage: {
-    messages: [
-      {
-        id: 1,
-        message: "H1",
-        photo: 'url(https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/reference_guide/cats_and_excessive_meowing_ref_guide/1800x1200_cats_and_excessive_meowing_ref_guide.jpg)'
-      },
-      {
-        id: 2,
-        message: "Hello!",
-        photo: 'url(https://focus.ua/storage/pub/images/2015/grumpy-cat_realgrumpycat-Instagram.jpg)'
-      },
-      {
-        id: 3,
-        message: "Haudi",
-        photo: 'url(https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg)'
-      },
-    ],
-    newMessage: '',
-    dialogs: [
-      {
-        id: 1,
-        name: "EE",
-      },
-      {
-        id: 2,
-        name: "JJ",
-      },
-      {
-        id: 3,
-        name: "WS",
-      },
-    ],
+  addPost () {
+    let newPost = {
+      id: 3,
+      message: this._state.profilePage.newPostText,
+      like : 0
+    };
+  
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = '';
+  
+    this._callSubscriber(this._state);
   },
-  aside : {
-    title : [
-      {
-        titlefriends: 'Best friends'
-      }
-    ],
-    friends : [
-      {
-        name: 'Chandler',
-        photo: 'https://post.healthline.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg'
-      },
-      {
-        name: 'Joey',
-        photo: 'https://www.cesarsway.com/wp-content/uploads/2019/10/AdobeStock_190562703.jpeg'
-      },
-      {
-        name: 'Ross',
-        photo: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/03/26125843/yorkshire-terrier-cover-500x486.jpg'
-      },
-      {
-        name: 'Gunther',
-        photo: 'https://cdn.cnn.com/cnnnext/dam/assets/180316113418-travel-with-a-dog-3-super-169.jpeg'
-      }
-    ]
+  
+  updateNewPostText (newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  
+  addMsg () {
+    let newMsg = {
+      id: 4,
+      message: this._state.dialogsPage.newMessage,
+      photo: 'url(https://focus.ua/storage/pub/images/2015/grumpy-cat_realgrumpycat-Instagram.jpg)'
+    }
+  
+    this._state.dialogsPage.messages.push(newMsg);
+    this._state.dialogsPage.newMessage = '';
+  
+    this._callSubscriber(this._state);
+  },
+  updateNewMessage (newMsg) {
+    this._state.dialogsPage.newMessage = newMsg;
+    this._callSubscriber(this._state);
+  },
+  subscribe (observer) {
+    this._callSubscriber = observer;
   }
-};
 
-export let addPost = () => {
-  let newPost = {
-    id: 3,
-    message: state.profilePage.newPostText,
-    like : 0
-  };
-
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = '';
-
-  rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export let addMsg = () => {
-  let newMsg = {
-    id: 4,
-    message: state.dialogsPage.newMessage,
-    photo: 'url(https://focus.ua/storage/pub/images/2015/grumpy-cat_realgrumpycat-Instagram.jpg)'
-  };
-
-  state.dialogsPage.messages.push(newMsg);
-  state.dialogsPage.newMessage = '';
-
-  rerenderEntireTree(state);
-}
-
-export let updateNewMessage = (newMsg) => {
-  state.dialogsPage.newMessage = newMsg;
-  rerenderEntireTree(state);
 };
 
 
-
-
-export default state;
+export default store;
+window.store = store;
